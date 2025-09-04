f4e1860f7044caa875117dfd3b9a77a7dffd0c4f
JohnMarkGerminal <jumjumescopete16>
2025-09-04 09:07:03 +0800

    Added interface parameters choice

--- factorial.js
+++ factorial.js
@@ -5,8 +5,23 @@ const rl = readline.createInterface({
     output:process.stdout
 })
 
-console.log("Factorial Application")"
+console.log("Factorial Application");
 
-console.log("1. Say hello");
+console.log("1. Say Hello");
 console.log("2. Compute Factorial");
 console.log("3. Exit");
+
+rl.question("Enter your choice (1-3)" , choice =>{
+    if(choice==="1"){
+        console.log("Hello There!");
+
+    } else if (choice==="2"){
+        console.log("Factorial");
+    }else if (choice==="3"){
+        console.log("Exit");
+    } else{ 
+        console.log("Invalid choice Please Try again")
+
+    }
+
+})
