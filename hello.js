94d1ab1ac0b966d4c9e899e753666202a99679e6
JohnMarkGerminal <jumjumescopete16@gmail.com>
2025-09-11 10:34:29 +0800

    Added Main Menu Factorial computeFactorial and BackMenu

---
+++ hello.js
@@ -0,0 +1,16 @@
+console.log("Hello World");
+
+num1 = 2;
+num2 = 3;
+
+sum= num1 + num2;
+diff= num1 - num2;
+prod = num1 * num2;
+qou = num1 / num2;
+
+console.log("The sum of " + num1 + " & " + num2 + " is " + sum);
+console.log("The difference of " + num1 + " & " + num2 + " is " + diff);
+console.log("The Product of " + num1 + " & " + num2 + " is " + prod);
+console.log("The Quotient of " + num1 + " & " + num2 + " is " + qou);
+
+console.log("this is the lab/factorial branch");
