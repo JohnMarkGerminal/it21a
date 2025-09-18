ae05dc45aa8d4ed3130d46340762d2ed387a3d35
JohnMarkGerminal <jumjumescopete16@gmail.com>
2025-09-18 08:49:58 +0800

    Add perfom ability as abstraction sample

--- hero.js
+++ hero.js
@@ -1,24 +1,92 @@
 class Hero{
+    #health;
+
+    constructor(name, health, attack){
+        this.name = name;
+        this.#health = health;
+        this.attack = attack;
+        this.items = [];
+    }
+
+    getHealth(){
+        return this.#health;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getAttack(){
+       console.log(this.attack);
+    }
+
+    getStats(){
+       console.log("\n");
+       console.log("Name: " + this.name);
+       console.log("Health: " + this.#health);
+       console.log("Attack: " + this.attack);
+    }
+
+    addItem(item){
+        this.items.push(item);
+    }
+
+    totalAttack(){
+        return this.attack + this.items.reduce((sum, i) => sum + i.bonusAttack, 0);
+
+    }    
 }
 
 class Warrior extends Hero{
     useAbility(){
-        console.log(`${this.name} uses power Strike`);
+        console.log(`${this.name} uses power Strike!`);
+        
     }
 }
 
 class Mage extends Hero{
-    constructor(name,health,attack,mana){
-        super(name,health,attack);
+    constructor(name, health, attack, mana){
+        super(name, health, attack);
         this.mana = mana;
     }
     useAbility(){
-        console.log(`${this.name} casts Fireball!`);
+        console.log(`${this.name} uses Fireball!`); 
+     
+    }
+
+}
+
+class Item{
+    constructor(name, bonusAttack, ){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+        
     }
 }
 
-const warrior = new Warrior("Thorin",100,10);
-warrior.useAbility();
+const sword = new Item("Sword", 5);
+const staff = new Item("Staff", 3);
 
-const mage = new Mage("Gandalf",80,8,50);
-mage.useAbility();
+const Thorin = new Warrior("Thorin", 100, 100);
+Thorin.getName();
+Thorin.getAttack();
+Thorin.addItem(sword);
+console.log(Thorin.totalAttack());
+
+
+
+
+const Gandalf = new Mage("Gandalf", 80, 50, 9999);
+Gandalf.getName();
+Gandalf.getAttack();
+Gandalf.addItem(staff);
+console.log(Gandalf.totalAttack());
+
+
+function performAbility(hero){
+    console.log("\n");
+    hero.useAbility();
+}
+
+performAbility(Thorin);
+performAbility(Gandalf);
